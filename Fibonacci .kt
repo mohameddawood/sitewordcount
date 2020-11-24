7ae77c635a96bf90d4bb9bc5669b00be1e306dba
Mohameddawood1994 <58386433+Mohameddawood1994@users.noreply.github.com>
2020-11-24 13:04:47 +0200

    Update Fibonacci .kt

--- Fibonacci .kt
+++ Fibonacci .kt
@@ -1,13 +1,13 @@
 package com.mctech.single_activity.presentation
 
 
-private fun recursiveFabonacci(fabNumber: Int): Int {
+private fun recursiveFibonacci(fabNumber: Int): Int {
     if (fabNumber <= 1)
         return fabNumber;
     return recursiveFabonacci(fabNumber - 1) + recursiveFabonacci(fabNumber - 2);
 }
 
-private fun iterativeFabonacci(fabNumber: Int): Int {
+private fun iterativeFibonacci(fabNumber: Int): Int {
     val numbers = arrayListOf<Int>()
     var i = 2
     numbers.add(0)
@@ -22,7 +22,7 @@ private fun iterativeFabonacci(fabNumber: Int): Int {
 
 
 fun main(args: Array<String>) {
-    println("fab recursive is ${recursiveFabonacci(9)}")
-    println("fab iterative is ${iterativeFabonacci(9)}")
+    println("fab recursive is ${recursiveFibonacci(9)}")
+    println("fab iterative is ${iterativeFibonacci(9)}")
 
 }
